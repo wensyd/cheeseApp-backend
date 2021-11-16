a5e879fc18f8b5878ba0d7ad73847e8ba57464d1
wensydesousa <wensys013@gmail.com>
2021-11-15 19:01:44 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,111 @@
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+// get .env variables
+require("dotenv").config();
+// pull PORT from .env, give default value of 3000
+// pull DATABASE_URL from .env
+const { PORT = 3000, DATABASE_URL } = process.env;
+// import express
+const express = require("express");
+// create application object
+const app = express();
+// import mongoose
+const mongoose = require("mongoose");
+// import middlware
+const cors = require("cors");
+const morgan = require("morgan");
+
+///////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////////
+// Establish Connection
+mongoose.connect(DATABASE_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+// Connection Events
+mongoose.connection
+  .on("open", () => console.log("Your are connected to mongoose"))
+  .on("close", () => console.log("Your are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
+
+
+///////////////////////////////
+// MODELS
+////////////////////////////////
+
+const CheeseSchema = new mongoose.Schema({
+    name: String,
+    countryOfOrigin: String,
+    image: String,
+});
+
+const Cheese = mongoose.model("Cheese", CheeseSchema);
+
+///////////////////////////////
+// MiddleWare
+////////////////////////////////
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+
+///////////////////////////////
+// ROUTES
+////////////////////////////////
+// create a test route
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+// cheese index route
+app.get("/cheese", async (req, res)=> {
+    try {
+        // send all cheese
+        res.json(await Cheese.find({}));
+    } catch (error) {
+        // send error
+        res.status(400).json(error);
+    }
+}) 
+
+// cheese create route
+app.post("/cheese", async (req, res) => {
+    try {
+      // create a new cheese
+      res.json(await Cheese.create(req.body));
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  });
+
+
+// CHEESE Update ROUTE
+app.put("/cheese/:id", async (req, res) => {
+    try {
+      // send all cheese
+      res.json(
+        await Cheese.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      );
+    } catch (error) {
+      //send error
+      res.status(400).json(error);
+    }
+  });
+  
+  // CHEESE Destroy ROUTE
+  app.delete("/cheese/:id", async (req, res) => {
+    try {
+      // send all Cheese
+      res.json(await Cheese.findByIdAndRemove(req.params.id));
+    } catch (error) {
+      //send error
+      res.status(400).json(error);
+    }
+  });
+
+
+///////////////////////////////
+// LISTENER
+////////////////////////////////
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
